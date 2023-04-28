9b77a445fafb3ca1e9287fcd5573b944cf407ea4
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-04-28 19:35:31 -0300

    Delete footium-eth-shareable directory

--- footium-eth-shareable/README.md
+++
@@ -1,6 +0,0 @@
-# `footium-eth`
-
-`footium-eth` is responsible for the smart contract code. The smart contracts
-are minimal and take on as few roles as possible.
-
-- [Technical Documentation](./contracts/technical-docs)
--- footium-eth-shareable/contracts/interfaces/IFootiumPlayer.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
-
-import {IERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
-
-interface IFootiumPlayer is IERC721Upgradeable {
-    function safeMint(address) external returns (uint256);
-}
